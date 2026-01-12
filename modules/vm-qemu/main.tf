4cf1e9a8e3f5ed01e3ab6a5c1cb974d64fe5afb7
jeremiah9980 <49728959+jeremiah9980@users.noreply.github.com>
2026-01-12 15:51:54 -0600

    Update main.tf

--- modules/vm-qemu/main.tf
+++ modules/vm-qemu/main.tf
@@ -1,52 +1,35 @@
-resource "proxmox_virtual_environment_vm" "vm" {
-  count     = var.vm_count
-  name      = "tf-vm-${count.index}"
-  node_name = var.node_name
+resource "proxmox_vm_qemu" "vm" {
+  count       = var.vm_count
+  name        = format("%s-%02d", var.vm_name_prefix, count.index + 1)
+  target_node = var.target_node
 
-  # Clone from an existing Proxmox VM template by name
-  clone {
-    vm_name = var.clone_template_name
-    full    = true
-  }
+  clone      = tostring(var.template_vmid)
+  full_clone = true
 
-  agent {
-    enabled = true
-  }
+  agent   = 1
+  os_type = "cloud-init"
 
-  cpu {
-    cores = 2
-    type  = "host"
-  }
+  cores  = 2
+  memory = 2048
 
-  memory {
-    dedicated = 2048
-  }
+  ciuser  = var.ci_user
+  sshkeys = var.ssh_public_key
 
-  initialization {
-    user_account {
-      username = var.ci_user
-      keys     = [var.ssh_public_key]
-    }
-
-    # Optional: DHCP on vmbr0
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-
-  network_device {
-    bridge = "vmbr0"
-    model  = "virtio"
-  }
+  scsihw = "virtio-scsi-pci"
 
   disk {
-    datastore_id = var.datastore_disks
-    interface    = "scsi0"
-    size         = 20
-    file_format  = "raw"
+    slot    = 0
+    size    = "40G"
+    type    = "scsi"
+    storage = var.storage_pool
   }
 
-  scsi_hardware = "virtio-scsi-single"
+  network {
+    model  = "virtio"
+    bridge = var.bridge
+    tag    = var.vlan_tag > 0 ? var.vlan_tag : null
+  }
+
+  onboot = true
 }
+
