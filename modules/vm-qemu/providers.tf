4885de9660dc51b292e72718b5ec2238eaade6de
jeremiah9980 <49728959+jeremiah9980@users.noreply.github.com>
2026-01-12 14:55:44 -0600

    Update providers.tf

--- modules/vm-qemu/providers.tf
+++ modules/vm-qemu/providers.tf
@@ -2,6 +2,15 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
+      version = ">= 0.93.0"
     }
   }
+}
+
+provider "proxmox" {
+  endpoint = "https://47.163.25.30:8006/api2/json"
+  insecure = true
+
+  username = var.pm_user
+  password = var.pm_password
 }
