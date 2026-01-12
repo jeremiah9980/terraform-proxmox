5d6beb7ae3c64252d3885832043e49f3aebf3c27
jeremiah9980 <49728959+jeremiah9980@users.noreply.github.com>
2026-01-12 16:07:08 -0600

    Update outputs.tf

--- modules/vm-qemu/outputs.tf
+++ modules/vm-qemu/outputs.tf
@@ -1,3 +1,9 @@
 output "vm_names" {
-  value = [for v in proxmox_virtual_environment_vm.vm : v.name]
+  description = "Names of created VMs"
+  value       = [for v in proxmox_vm_qemu.vm : v.name]
+}
+
+output "vm_ids" {
+  description = "VMIDs of created VMs"
+  value       = [for v in proxmox_vm_qemu.vm : v.vmid]
 }
